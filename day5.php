f2872f862b042f2ce8e5765cc0ec2d5b2cedffb4
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2023-10-13 19:27:33 +0800

    Add files via upload

---
+++ day5.php
@@ -0,0 +1,13 @@
+<?php
+// Mengecek apakah nilai variabel adalah true atau false
+$nilai = true;
+
+if ($nilai) {
+  echo "Nilai variabel adalah true";
+} else {
+  echo "Nilai variabel adalah false";
+}
+
+// Hasil: Nilai variabel adalah true
+
+?>
