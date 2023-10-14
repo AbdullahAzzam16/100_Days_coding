c4a947a74dfc58e43754798aeaa716cd69c7a5e5
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2023-10-14 19:14:26 +0800

    Add files via upload

---
+++ day6.php
@@ -0,0 +1,17 @@
+<?php
+
+// Variabel integer
+$a = 7;
+
+// Variabel string
+$b = "Sabtu";
+
+// Variabel boolean
+$c = true;
+
+echo "Hari ini adalah harike $a";
+echo "<br>";
+echo "Dan merupakan hari $b";
+echo "<br>";
+echo " Tentu saja $c";
+?>
