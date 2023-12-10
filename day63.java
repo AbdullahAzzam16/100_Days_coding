8ee9c01ee6d54df9ddd8a32449e6f966b8119901
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2023-12-10 11:36:09 +0800

    Add files via upload

---
+++ day63.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class day63 {
+    public static void main(String[] args) {
+        int [] angka = {1, 3, 5, 7, 9};
+
+        System.out.println(angka[2]);
+    }   
+}
