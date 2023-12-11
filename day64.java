0d6027a00ff9b254cbee0bb9cace87dc77bad2eb
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2023-12-11 09:35:40 +0800

    Array

---
+++ day64.java
@@ -0,0 +1,9 @@
+public class day64 {
+    public static void main(String[] args) {
+        String [] hewan = {"Kuda", "Ayam","Kambing", "Anjing"};
+
+        for (int i = 0; i < hewan.length; i++) {
+            System.out.println(hewan[i]);
+        }
+    }
+}
