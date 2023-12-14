5629ae53c8528aa1de77b03b89ece311176b99d8
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2023-12-14 08:14:57 +0800

    Array

---
+++ day67.java
@@ -0,0 +1,10 @@
+public class day67 {
+    public static void main(String[] args) {
+        int [] angka = {1, 3, 5, 7}; 
+
+        for(int a : angka){
+            System.out.println(a);
+        }
+    }
+    
+}
