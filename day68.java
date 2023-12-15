1cb72e2553129feb597627ff3b6208e38fc543d2
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2023-12-15 20:24:54 +0800

    Array 2 Dimensi

---
+++ day68.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class day68 {
+ public static void main(String[] args) {
+    String[][] umur = {
+        {"Yanto", "16"},
+        {"Balan", "15"},
+        {"Anas", "16"}
+    };
+    for (int i = 0; i < umur.length; i++) {
+        System.out.println("Nama :"+ umur[i][0]);
+        System.out.println("Umur :"+ umur[i][1]);
+    }
+ }
+}
