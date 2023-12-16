f132294ce3d99c84f5dcdfa7acad20b5c8206ff3
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2023-12-16 16:04:55 +0800

    Add files via upload

---
+++ day69.java
@@ -0,0 +1,13 @@
+public class day69 {
+    public static void main(String[] args) {
+        String [][] buah = {
+            {"1", "Jeruk"},
+            {"2", "Apel"},
+            {"3", "Durian"},
+        };
+        String[] a = buah[1];
+        for (String b : a) {
+         System.out.print(b);   
+        }
+    }    
+}
