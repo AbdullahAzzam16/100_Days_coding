a95c5de8c9b3fe1789f27bcf9dc59c8d753e652e
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2023-12-22 07:04:41 +0800

    Add files via upload

---
+++ day75.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class day75 {
+     public static void main(String[] args) {
+    HashMap<Integer, String> hari = new HashMap<Integer, String>();
+
+    hari.put(1, "Senin");
+    hari.put(2, "Selasa");
+    hari.put(3, "Rabu");
+    hari.put(4, "Kamis");
+    hari.put(5, "Jumat");
+    hari.put(6, "Sabtu");
+    hari.put(7, "Minggu");
+
+    hari.put(7, "Ahad");
+    System.out.println(hari);
+    }
+}
