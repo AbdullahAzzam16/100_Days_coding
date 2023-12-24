2bf53e754370d2b862faa97d9a03f4510fd44295
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2023-12-24 16:49:25 +0800

    Add files via upload

---
+++ day77.java
@@ -0,0 +1,21 @@
+class day77{
+    static String nama = "Programku";
+    static String version = "1.0.0";
+
+    static void help(){
+        String nama = "Pencari Nilai";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Versi: " + version);
+    }
+
+    public static void main(String args[]){
+        
+        help();
+        
+        System.out.println("Nama: " + nama);
+        System.out.println("Versi: " + version);
+
+    }
+
+}
