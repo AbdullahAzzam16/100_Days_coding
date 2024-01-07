1a3f4706a5c0377eac1674169dd25e1ed1209c16
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2024-01-07 19:02:31 +0800

    Add files via upload

---
+++ day91.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class day91 {
+    public static void main(String[] args) {
+        int[] array = {5, 1, 3, 2, 4};
+
+        Arrays.sort(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+}
