659ba62f1de47d9a0f5e6a49664255e0d68c5133
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2024-01-12 12:07:58 +0800

    Add files via upload

---
+++ day96.java
@@ -0,0 +1,9 @@
+public class day96 {
+    public static void main(String[] args) {
+        String[] array = {"satu", "dua", "tiga", "4", "5"};
+        
+        for (String value : array) {
+            System.out.println(value);
+        }
+    }
+}
