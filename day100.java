3979fa953ec3a397c670c821f90b8a2a17484ee3
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2024-01-16 09:19:00 +0800

    Add files via upload

---
+++ day100.java
@@ -0,0 +1,17 @@
+public class day100 {
+    public static void main(String[] args) {
+        int number = 5;
+        long factorial = calculateFactorial(number);
+
+        System.out.println("Faktorial dari " + number + " adalah: " + factorial);
+    }
+
+    static long calculateFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
